1f53878e738822a83c3596f172cc4d29f8bb6711
Juha Rajamäki <juhatra@gmail.com>
2015-02-25 15:40:34 +0200

    added more exciting games bullomx15X

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# shares
+# App for tracking Oil, gold, Dax etc. share prices in real time
+
+# Example usage for certain share
+"php /certfificate/bullolja.php"
---
+++ certificates/bulldax-x10.php
@@ -0,0 +1,5 @@
+<?php
+        $title 	= 'BULL DAX X10C';
+        $url 	= 'https://www.netfonds.no/quotes/ppaper.php?paper=BULL-DAX-X10-BNP.NGM';
+
+	include('app/engine.php');
