ad7b0f8eaf35f6334c549ad0f4dea6e3e16481c5
Juha Rajamäki <juhatra@gmail.com>
2015-04-20 13:00:47 +0300

    naming

--- warrants/NOK5L-7-26CZB.php
+++ warrants/NOK5L-7-26CZB.php
@@ -1,5 +1,5 @@
 <?php
-        $title 		= 'NOKIA - NOK5L-7-26CZB';
+        $title 		= 'NOK5L-7-26 CZB';
 	$quote_date	= '18.12.2015';
         $url 		= 'http://www.netfonds.no/quotes/ppaper.php?paper=NOK5L-7-26CZB.NGM';
 	$engine 	= '1';
