50f5068547290495f4243861995861fc01b45545
Juha Rajamäki <juhatra@gmail.com>
2015-04-20 11:03:14 +0300

    layout fixes

--- certificates/bullolja-x5c.php
+++ certificates/bullolja-x5c.php
@@ -1,3 +1,3 @@
 <?php
-        $title 	= 'Bull Olja X5 C';
+        $title 	= 'BULL OLJA X5 C';
         $url 	= 'https://www.netfonds.no/quotes/ppaper.php?paper=BULL-OLJA-X5-C.NGM';
--- exchange-trade-notes/bullseadrill-x3-cz.php
+++ exchange-trade-notes/bullseadrill-x3-cz.php
@@ -1,3 +1,3 @@
 <?php
-        $title 	= 'Bull SeaDrill';
+        $title 	= 'BULL SEADRILL X3 CZ';
         $url 	= 'http://www.netfonds.no/quotes/ppaper.php?paper=BULL-SDRL-X3-CZ.NGM';
